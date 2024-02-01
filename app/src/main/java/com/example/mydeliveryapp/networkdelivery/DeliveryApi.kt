6821cc069c9a6580137b604c3d889549228c4d01
Simon Francisco <121758120+SimonFrancisco@users.noreply.github.com>
2024-02-01 09:17:00 +0700

    Added @POST ("delivery/calc") and created a class for the Body and result expected!

--- app/src/main/java/com/example/mydeliveryapp/networkdelivery/DeliveryApi.kt
+++ app/src/main/java/com/example/mydeliveryapp/networkdelivery/DeliveryApi.kt
@@ -4,7 +4,11 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 private const val BASE_URL = "https://shift-backend.onrender.com/"
 
@@ -16,17 +20,19 @@ private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
-
 interface DeliveryApiService {
     @GET("delivery/points")
     suspend fun getPoints(): DeliveryPoints
-
     @GET("delivery/package/types")
     suspend fun getTypes(): DeliveryTypes
+    @POST("delivery/calc")
+    suspend fun getCalculation(
+       @Body calculateBody: CalculateBody
+    ):DeliveryCalculateList
 
 }
 
-object DeliveryApi{
+object DeliveryApi {
     val retrofitService: DeliveryApiService by lazy {
         retrofit.create((DeliveryApiService::class.java))
     }
---
+++ app/src/main/java/com/example/mydeliveryapp/networkdelivery/DeliveryCalculateData.kt
@@ -0,0 +1,19 @@
+package com.example.mydeliveryapp.networkdelivery
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.RawValue
+import java.io.Serializable
+@Parcelize
+data class DeliveryCalculateList(
+    val options:@RawValue List<Option>,
+    val success: Boolean
+):Parcelable,Serializable
+@Parcelize
+data class Option(
+    val days: Int,
+    val id: String,
+    val name: String,
+    val price: Int,
+    val type: String
+):Parcelable
